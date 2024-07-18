f31ce52a6aa9179c678e4a91d28b1761d755eeeb
Havid <havidabdilah01@gmail.com>
2024-07-18 20:18:47 +0700

    modified:   controllers/RefreshToken.js 	modified:   controllers/Users.js 	new file:   note.txt 	modified:   request.rest

--- controllers/Users.js
+++ controllers/Users.js
@@ -2,34 +2,6 @@ import Users from "../models/UserModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const getUsers = async(req, res) => {
-    try {
-        const users = await Users.findAll({
-            attributes:['id','name','email']
-        });
-        res.json(users);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-export const Register = async(req, res) => {
-    const { name, email, password, confPassword } = req.body;
-    if(password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok"});
-    const salt = await bcrypt.genSalt();
-    const hashPassword = await bcrypt.hash(password, salt);
-    try {
-        await Users.create({
-            name: name,
-            email: email,
-            password: hashPassword
-        });
-        res.json({msg: "Register Berhasil"});
-    } catch (error) {
-        console.log(error);
-    }
-}
-
 export const Login = async(req, res) => {
     try {
         const user = await Users.findAll({
@@ -38,7 +10,7 @@ export const Login = async(req, res) => {
             }
         });
         const match = await bcrypt.compare(req.body.password, user[0].password);
-        if(!match) return res.status(400).json({msg: "Wrong Password"});
+        if(!match) return res.status(400).json({msg: "password salah"});
         const userId = user[0].id;
         const name = user[0].name;
         const email = user[0].email;
@@ -63,21 +35,3 @@ export const Login = async(req, res) => {
     }
 }
 
-export const Logout = async(req, res) => {
-    const refreshToken = req.cookies.refreshToken;
-    if(!refreshToken) return res.sendStatus(204);
-    const user = await Users.findAll({
-        where:{
-            refresh_token: refreshToken
-        }
-    });
-    if(!user[0]) return res.sendStatus(204);
-    const userId = user[0].id;
-    await Users.update({refresh_token: null},{
-        where:{
-            id: userId
-        }
-    });
-    res.clearCookie('refreshToken');
-    return res.sendStatus(200);
-}
