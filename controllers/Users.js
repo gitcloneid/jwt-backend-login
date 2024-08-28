c2a060a325ec81d4db7918a8a206a408fdcb3c69
havidabd19 <152508243+havidabd19@users.noreply.github.com>
2024-08-28 19:34:49 +0700

    Update Users.js

--- controllers/Users.js
+++ controllers/Users.js
@@ -38,7 +38,7 @@ export const Login = async(req, res) => {
             }
         });
         const match = await bcrypt.compare(req.body.password, user[0].password);
-        if(!match) return res.status(400).json({msg: "Wrong Password"});
+        if(!match) return res.status(400).json({msg: "Password Salah"});
         const userId = user[0].id;
         const name = user[0].name;
         const email = user[0].email;
